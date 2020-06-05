6ae1c9ed543a9b34ceda29f5d3b57a8bc536aca2
Emre <emrem5979@gmail.com>
2020-06-05 22:38:26 +0300

    customer router CRUD operations completed

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysqlConnection = require('../connection');
+const { v4: uuidv4 } = require('uuid');
 
 /* GET users listing. */
 router.get('/', (req, res) => {
@@ -12,6 +13,86 @@ router.get('/', (req, res) => {
     else {
       console.log('Execution success')
     res.json(result);
+    }
+  });
+});
+
+//create customer
+router.post('/add', (req, res) =>{
+  const uuid = uuidv4();
+  console.log(uuid);
+  const post  = {
+    id_customer: uuid, 
+    customerName: req.body.customerName,
+    customerLastName: req.body.customerLastName,
+    customerAge: req.body.customerAge,
+    customerPhoneNumber: req.body.customerPhoneNumber,
+    customerEmail: req.body.customerEmail ,
+    customerState: req.body.customerState 
+  };
+  console.log(req.body);
+
+  let query = 'INSERT INTO mydb.customer SET ?';
+  mysqlConnection.query(query, post, (err, result) => {
+    if(err){
+      console.log(err);
+    }
+    else {
+      console.log('This customer added to database ! !');
+      res.json('Customer is added to database');
+    }
+  });
+});
+
+//delete customer
+router.delete('/delete', (req, res) => {
+  console.log(req.body);
+  let query = 'DELETE FROM customer WHERE id_customer = ?';
+  mysqlConnection.query(query, [req.body.id_customer], (err, result) => {
+    if(err){
+      console.log(err);
+      res.status(500).send(err);
+    }
+    else{
+      console.log('Deleted customer');
+      res.send('Customer deleted from database');
+    }
+  });
+});
+
+// get a customer which is selected
+router.get('/:id', (req, res) => {
+  let query = 'SELECT * FROM mydb.customer WHERE id_customer = ?';
+  mysqlConnection.query(query,[req.params.id], (err, result) => {
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+   }
+  });
+});
+//update customer
+router.put('/update/:id', (req, res) => {
+  const post  = {
+    id_customer: req.body.id_customer, 
+    customerName: req.body.customerName,
+    customerLastName: req.body.customerLastName,
+    customerAge: req.body.customerAge,
+    customerPhoneNumber: req.body.customerPhoneNumber,
+    customerEmail: req.body.customerEmail ,
+    customerState: req.body.customerState 
+  };
+  console.log(req.body);
+  console.log(req.params.id);
+  let query = 'UPDATE mydb.customer SET ? WHERE id_customer = ?';
+  mysqlConnection.query(query, [post, req.params.id], (err, result) => {
+    if(err){
+      console.log(err);
+    }
+    else {
+      console.log('This customer updated and added to database ! !');
+      res.json('Customer is updated and added database');
     }
   });
 });
